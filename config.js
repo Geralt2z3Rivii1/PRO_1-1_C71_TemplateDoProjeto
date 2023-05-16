a80cc6c1bf6047d2ccde328ed2d946923ed2d111
Vinicius de Brito <88459597+Geralt2z3Rivii1@users.noreply.github.com>
2023-05-16 16:15:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,15 @@ require("@firebase/firestore");
 
 var firebaseConfig = {
   //Cole sua Configuração do Firebase aqui
-  
+  apiKey: "AIzaSyCt6IBZTWFl188U7OWfaIfdZsrzCDGM7QU",
+  authDomain: "cp71-770e7.firebaseapp.com",
+  projectId: "cp71-770e7",
+  storageBucket: "cp71-770e7.appspot.com",
+  messagingSenderId: "464252195797",
+  appId: "1:464252195797:web:94419b02532a95ff3a235c"
 };
+  
+
 
 firebase.initializeApp(firebaseConfig);
 
